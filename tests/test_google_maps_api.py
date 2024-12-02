fcdfa82435d6e4da6042c603d9ed6512315e72ac
Danil <danig1991@mail.ru>
2024-12-02 18:19:07 +0300

    ДЗ: Метод Get.

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -2,7 +2,16 @@ from untils.api import GoogleMapsApi
 
 
 class TestGoogleMapsApi:
+    place_id = ""
 
-    def test_create_new_place(self):
+    @classmethod
+    def test_create_new_place(cls):
         print("\nМетод POST")
-        GoogleMapsApi.create_new_place()
+        new_place = GoogleMapsApi.create_new_place()
+        cls.place_id = new_place.json()["place_id"]
+
+    @classmethod
+    def test_get_place(cls):
+        print("\nМетод GET")
+        print(f"Запрос по place_id = {cls.place_id}")
+        GoogleMapsApi.get_place(cls.place_id)
--- untils/api.py
+++ untils/api.py
@@ -1,4 +1,10 @@
-from configurations.configurations import BASE_URL, POST_RESOURCE, KEY, body_to_creating_location
+from configurations.configurations import (
+    BASE_URL,
+    POST_RESOURCE,
+    GET_RESOURCE,
+    KEY,
+    body_to_creating_location
+)
 from untils.http_methods import HttpMethods
 
 
@@ -12,3 +18,13 @@ class GoogleMapsApi:
         result_post = HttpMethods.post(url=post_url, body=body_to_creating_location())
         print(f"Ответ сервера:\n{result_post.json()}")
         return result_post
+
+    # получить локацию
+    @staticmethod
+    def get_place(place_id):
+        get_url = BASE_URL + GET_RESOURCE + KEY + "&place_id=" + place_id
+        print(f"URL для получения локации:\n{get_url}")
+
+        result_get = HttpMethods.get(url=get_url)
+        print(f"Ответ сервера:\n{result_get.json()}")
+        return result_get
