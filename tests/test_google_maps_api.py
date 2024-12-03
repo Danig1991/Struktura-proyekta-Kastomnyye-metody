05f37d7303f5e9a4d242c7e37be4eee8e157a889
Danil <danig1991@mail.ru>
2024-12-03 13:09:08 +0300

    ДЗ: Метод Post/Get/Put/Delete.

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -2,28 +2,24 @@ from untils.api import GoogleMapsApi
 
 
 class TestGoogleMapsApi:
-    place_id = ""
 
-    @classmethod
-    def test_create_new_place(cls):
+    # тестирование методов api
+    def test_methods_api(self):
         print("\nМетод POST")
         new_place = GoogleMapsApi.create_new_place()
-        cls.place_id = new_place.json()["place_id"]
+        place_id = new_place.json()["place_id"]
 
-    @classmethod
-    def test_get_place(cls, method="POST"):
-        print(f"\nМетод GET, после метода {method}")
-        print(f"Запрос по place_id = {cls.place_id}")
-        GoogleMapsApi.get_place(cls.place_id)
+        print("\nМетод GET, после метода POST")
+        GoogleMapsApi.get_place(place_id)
 
-    @classmethod
-    def test_put_place(cls):
         print("\nМетод PUT")
-        GoogleMapsApi.put_place(cls.place_id)
-        cls.test_get_place(method="PUT")
+        GoogleMapsApi.put_place(place_id)
 
-    @classmethod
-    def test_delete_place(cls):
+        print("\nМетод GET, после метода PUT")
+        GoogleMapsApi.get_place(place_id)
+
         print("\nМетод DELETE")
-        GoogleMapsApi.delete_place(cls.place_id)
-        cls.test_get_place(method="DELETE")
+        GoogleMapsApi.delete_place(place_id)
+
+        print("\nМетод GET, после метода DELETE")
+        GoogleMapsApi.get_place(place_id)
