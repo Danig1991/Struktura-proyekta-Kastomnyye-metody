32f530c036eaa77464efb814f50bc2de02bb7b97
Danil <danig1991@mail.ru>
2024-12-03 13:37:02 +0300

    ДЗ: Метод для проверки статус кода.

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,4 +1,5 @@
 from untils.api import GoogleMapsApi
+from untils.checking import Checking
 
 
 class TestGoogleMapsApi:
@@ -7,20 +8,26 @@ class TestGoogleMapsApi:
     @staticmethod
     def test_methods_api():
         print("\nМетод POST")
-        new_place = GoogleMapsApi.create_new_place()
-        place_id = new_place.json()["place_id"]
+        result_post = GoogleMapsApi.create_new_place()
+        place_id = result_post.json()["place_id"]
+        Checking.check_status_code(result_post, 200)
 
         print("\nМетод GET, после метода POST")
-        GoogleMapsApi.get_place(place_id)
+        result_get = GoogleMapsApi.get_place(place_id)
+        Checking.check_status_code(result_get, 200)
 
         print("\nМетод PUT")
-        GoogleMapsApi.put_place(place_id)
+        result_put = GoogleMapsApi.put_place(place_id)
+        Checking.check_status_code(result_put, 200)
 
         print("\nМетод GET, после метода PUT")
-        GoogleMapsApi.get_place(place_id)
+        result_get = GoogleMapsApi.get_place(place_id)
+        Checking.check_status_code(result_get, 200)
 
         print("\nМетод DELETE")
-        GoogleMapsApi.delete_place(place_id)
+        result_delete = GoogleMapsApi.delete_place(place_id)
+        Checking.check_status_code(result_delete, 200)
 
         print("\nМетод GET, после метода DELETE")
-        GoogleMapsApi.get_place(place_id)
+        result_get = GoogleMapsApi.get_place(place_id)
+        Checking.check_status_code(result_get, 404)
